e7b45324ea699dbd704fd960a0dca999c8f77640
hituyuu <2256765706@qq.com>
2018-09-04 18:48:33 +0800

    小组项目

---
+++ logistics-manage/logistics-manage-web/src/main/java/com/qf/logistics/manage/web/LoginAction.java
@@ -0,0 +1,46 @@
+package com.qf.logistics.manage.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Administrator on 2018/8/30.
+ */
+@Controller
+//@RequestMapping("/user")
+public class LoginAction {
+/*   规定请求方式必须为get
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }*/
+
+    //    每个访问页面都配一个控制器的话,工作量太繁琐,我们可以将同级路径写成同一个
+//    上述两个控制器都可以分别工作,但是可以整合成下面一个
+    @RequestMapping(value = "/{target}",method = RequestMethod.GET)
+    public String login(@PathVariable String target){
+        System.out.println("访问的路径资源->"+target);
+        return target;
+    }
+
+
+    @RequestMapping(value = "pages/{target}",method = RequestMethod.GET)
+    public String pages(@PathVariable String target){
+        System.out.println("访问的路径资源->pages/"+target);
+        return "pages/"+target;
+    }
+
+    @RequestMapping(value = "pages/{target1}/{target2}",method = RequestMethod.GET)
+    public String pages(@PathVariable String target1,@PathVariable String target2){
+        System.out.println("访问的路径资源->pages/"+target1+"/"+target2);
+        return "pages/"+target1+"/"+target2;
+    }
+}
